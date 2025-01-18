8b9dc00a8964889de177a752d3e020f9efed898a
Brylie Christopher Oxley <brylie@protonmail.com>
2025-01-18 14:55:45 +0200

    Add scales for all major keys

---
+++ app/scripts/exercises/scales.gd
@@ -0,0 +1,132 @@
+# Right hand ascending scales with correct fingerings
+var c_major_rh_1oct = [
+	["C4", 1], # Thumb
+	["D4", 2], # Index
+	["E4", 3], # Middle
+	["F4", 1], # Thumb (thumb under)
+	["G4", 2], # Index
+	["A4", 3], # Middle
+	["B4", 4], # Ring
+	["C5", 5] # Pinky
+]
+
+var g_major_rh_1oct = [
+	["G4", 1], # Thumb
+	["A4", 2], # Index
+	["B4", 3], # Middle
+	["C5", 1], # Thumb (thumb under)
+	["D5", 2], # Index
+	["E5", 3], # Middle
+	["F#5", 4], # Ring
+	["G5", 5] # Pinky
+]
+
+var d_major_rh_1oct = [
+	["D4", 1], # Thumb
+	["E4", 2], # Index
+	["F#4", 3], # Middle
+	["G4", 1], # Thumb (thumb under)
+	["A4", 2], # Index
+	["B4", 3], # Middle
+	["C#5", 4], # Ring
+	["D5", 5] # Pinky
+]
+
+var a_major_rh_1oct = [
+	["A4", 1], # Thumb
+	["B4", 2], # Index
+	["C#5", 3], # Middle
+	["D5", 1], # Thumb (thumb under)
+	["E5", 2], # Index
+	["F#5", 3], # Middle
+	["G#5", 4], # Ring
+	["A5", 5] # Pinky
+]
+
+var e_major_rh_1oct = [
+	["E4", 1], # Thumb
+	["F#4", 2], # Index
+	["G#4", 3], # Middle
+	["A4", 1], # Thumb (thumb under)
+	["B4", 2], # Index
+	["C#5", 3], # Middle
+	["D#5", 4], # Ring
+	["E5", 5] # Pinky
+]
+
+var b_major_rh_1oct = [
+	["B4", 1], # Thumb
+	["C#5", 2], # Index
+	["D#5", 3], # Middle
+	["E5", 1], # Thumb (thumb under)
+	["F#5", 2], # Index
+	["G#5", 3], # Middle
+	["A#5", 4], # Ring
+	["B5", 5] # Pinky
+]
+
+var f_sharp_major_rh_1oct = [
+	["F#4", 1], # Thumb
+	["G#4", 2], # Index
+	["A#4", 3], # Middle
+	["B4", 1], # Thumb (thumb under)
+	["C#5", 2], # Index
+	["D#5", 3], # Middle
+	["E#5", 4], # Ring
+	["F#5", 5] # Pinky
+]
+
+var c_sharp_major_rh_1oct = [
+	["C#4", 1], # Thumb
+	["D#4", 2], # Index
+	["E#4", 3], # Middle
+	["F#4", 1], # Thumb (thumb under)
+	["G#4", 2], # Index
+	["A#4", 3], # Middle
+	["B#4", 4], # Ring
+	["C#5", 5] # Pinky
+]
+
+var f_major_rh_1oct = [
+	["F4", 1], # Thumb
+	["G4", 2], # Index
+	["A4", 3], # Middle
+	["Bb4", 4], # Ring
+	["C5", 1], # Thumb (thumb under)
+	["D5", 2], # Index
+	["E5", 3], # Middle
+	["F5", 4] # Ring
+]
+
+var b_flat_major_rh_1oct = [
+	["Bb4", 1], # Thumb
+	["C5", 2], # Index
+	["D5", 3], # Middle
+	["Eb5", 1], # Thumb (thumb under)
+	["F5", 2], # Index
+	["G5", 3], # Middle
+	["A5", 4], # Ring
+	["Bb5", 5] # Pinky
+]
+
+var e_flat_major_rh_1oct = [
+	["Eb4", 1], # Thumb
+	["F4", 2], # Index
+	["G4", 3], # Middle
+	["Ab4", 1], # Thumb (thumb under)
+	["Bb4", 2], # Index
+	["C5", 3], # Middle
+	["D5", 4], # Ring
+	["Eb5", 5] # Pinky
+]
+
+var a_flat_major_rh_1oct = [
+	["Ab4", 1], # Thumb
+	["Bb4", 2], # Index
+	["C5", 3], # Middle
+	["Db5", 1], # Thumb (thumb under)
+	["Eb5", 2], # Index
+	["F5", 3], # Middle
+	["G5", 4], # Ring
+	["Ab5", 5] # Pinky
+]
