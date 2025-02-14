7239d6b79fd81588c6aa56ecefdf42ad6bff444d
Brylie Christopher Oxley <brylie@protonmail.com>
2025-02-14 07:05:09 +0200

    Remove Colors autoload; add maroon

--- app/scripts/constants/colors.gd
+++ app/scripts/constants/colors.gd
@@ -3,3 +3,4 @@ extends Node
 const LEFT_HAND_COLOR = Color(0.0, 0.5, 1.0, 1.0) # Blue
 const RIGHT_HAND_COLOR = Color(1.0, 0.8, 0.0, 1.0) # Yellow
 const FINGER_DISPLAY_BACKGROUND_COLOR = Color(0.1, 0.1, 0.1, 1.0) # Dark Gray
+const MAROON_COLOR = Color(0.5, 0, 0) # Deep red color resembling maroon
